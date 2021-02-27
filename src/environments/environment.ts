afa8fe294daf8b862c8e68717252f1cc24a36a2e
Viniciu Cristian <git.viniciu@gmail.com>
2021-02-27 15:52:29 -0300

    Delete lines environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,12 +3,4 @@ import { firebaseConfig } from './../fireBaseConfig';
 export const environment = {
   production: false,
   firebaseConfig: firebaseConfig
-  // {
-  //   apiKey: "AIzaSyD7TIdzJ-d8dpnNNVYeZ0JWD6fG9Z881JA",
-  //   authDomain: "applibrass.firebaseapp.com",
-  //   projectId: "applibrass",
-  //   storageBucket: "applibrass.appspot.com",
-  //   messagingSenderId: "85201740256",
-  //   appId: "1:85201740256:web:6e9c72691bfc3ac64fcb85"
-  // }
 };
